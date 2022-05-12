10ef21413370e38c5dab586042b04216f61048e6
Diogo Sousa <dfb.sousa@campus.fct.unl.pt>
2022-05-12 12:29:18 +0100

    Added input management actions

--- Scavenger_Hunt/Assets/Scripts/FPSCounter.cs
+++ Scavenger_Hunt/Assets/Scripts/FPSCounter.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class FPSCounter : MonoBehaviour
 {
     private int frameRate;
-    
+    private bool calculateFrames;
     // Update is called once per frame
     void Update()
     {
         //calculate frameRate
-        frameRate =(int)(1.0f / Time.unscaledDeltaTime);
+        if (calculateFrames)
+        {
+            frameRate = (int)(1.0f / Time.unscaledDeltaTime);
+        }
     }
 }
