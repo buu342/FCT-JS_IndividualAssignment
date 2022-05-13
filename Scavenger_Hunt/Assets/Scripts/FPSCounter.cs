6fe38c7936cf185ffad1776fad88abbb7781028a
Diogo Sousa <dfb.sousa@campus.fct.unl.pt>
2022-05-13 16:35:55 +0100

    Added FPS counter

--- Scavenger_Hunt/Assets/Scripts/FPSCounter.cs
+++ Scavenger_Hunt/Assets/Scripts/FPSCounter.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class FPSCounter : MonoBehaviour
 {
+    public PlayerInput playerControls;
+    private InputAction calculateFramesAction;
     private int frameRate;
     private bool calculateFrames;
+    
+    private void Awake() {
+        playerControls = new PlayerInput();
+    }
+
+    private void OnEnable() {
+
+        calculateFramesAction = playerControls.UI.FPSCounter;
+        calculateFramesAction.Enable();
+        calculateFramesAction.performed += calculateFramesTriggered;
+    }
+
+    private void OnDisable() {
+        calculateFramesAction.performed -= calculateFramesTriggered;
+         calculateFramesAction.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -11,6 +30,11 @@ public class FPSCounter : MonoBehaviour
         if (calculateFrames)
         {
             frameRate = (int)(1.0f / Time.unscaledDeltaTime);
+            Debug.Log(frameRate);
         }
+    }
+
+    private void calculateFramesTriggered(InputAction.CallbackContext context) {
+        calculateFrames = !calculateFrames;
     }
 }
