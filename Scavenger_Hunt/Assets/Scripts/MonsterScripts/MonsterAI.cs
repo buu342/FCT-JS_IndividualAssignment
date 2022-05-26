24e99ac31e47d61927b2b4cd715dabae55f243d0
Diogo Sousa <dfb.sousa@campus.fct.unl.pt>
2022-05-26 20:37:58 +0100

    Check distance of sound fixed

--- Scavenger_Hunt/Assets/Scripts/MonsterScripts/MonsterAI.cs
+++ Scavenger_Hunt/Assets/Scripts/MonsterScripts/MonsterAI.cs
@@ -19,8 +19,9 @@ public class MonsterAI : MonoBehaviour
     }
 
     void HearsSound(Vector3 origin, float distance) {
-        if(Vector3.Distance(origin, transform) < (distance + hearingDistance)) {
+        if(Vector3.Distance(origin, transform.position) < (distance + hearingDistance)) {
             hearsPlayer = true;
+            
         }
 
     }
