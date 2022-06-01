buu342/FCT-JS_IndividualAssignment

commit a2aed2bc0ebf97993d44c30c27988b2bd86dfea1

Added distance calculation of sound

Scavenger_Hunt/Assets/Scripts/MonsterScripts/MonsterAI.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class MonsterAI : MonoBehaviour
 {
-   
+    bool hearsPlayer;
+    public int hearingDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hearsPlayer = false;
     }
 
     // Update is called once per frame
@@ -17,9 +18,11 @@ public class MonsterAI : MonoBehaviour
         
     }
 
-    void HearsSound(float radius) {
-        //TODO: check intersections
-        Debug.Log("Checking sounds");
+    void HearsSound(Vector3 origin, float distance) {
+        if(Vector3.Distance(origin, transform) < (distance + hearingDistance)) {
+            hearsPlayer = true;
+        }
+
     }
 
 }

commit e978e89cabb382a26e4a416bb3ea95ef031186e2

Fixed compilation error introduced in a2aed2b

Scavenger_Hunt/Assets/Scripts/MonsterScripts/MonsterAI.cs
@@ -19,7 +19,7 @@ public class MonsterAI : MonoBehaviour
     }
 
     void HearsSound(Vector3 origin, float distance) {
-        if(Vector3.Distance(origin, transform) < (distance + hearingDistance)) {
+        if(Vector3.Distance(origin, transform.position) < (distance + hearingDistance)) {
             hearsPlayer = true;
         }
 