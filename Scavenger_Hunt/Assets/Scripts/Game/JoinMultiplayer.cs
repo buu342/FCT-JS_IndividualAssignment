d90b191c7b5ec22ad9eb57964c76d2ad22141248
Pedro <pmd.ribeiro@campus.fct.unl.pt>
2022-06-12 19:47:17 +0100

    0.101
    Fixed some logic

--- Scavenger_Hunt/Assets/Scripts/Game/JoinMultiplayer.cs
+++ Scavenger_Hunt/Assets/Scripts/Game/JoinMultiplayer.cs
@@ -6,8 +6,7 @@ using TMPro;
 public class JoinMultiplayer: MonoBehaviourPunCallbacks{
 public TMP_InputField createInput;
 public TMP_InputField JoinInput;    
-public int PlayerCount=0;
- 
+
     public void createRoom(){
         PhotonNetwork.CreateRoom(createInput.text);
 
@@ -18,23 +17,13 @@ public int PlayerCount=0;
     }
 
     
-    void Update(){
-        if(PlayerCount==1)
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 2){
-            PhotonNetwork.LoadLevel("SampleScene");
-        }
-    }
+   
  
     public override void OnJoinedRoom(){
-        PlayerCount=1;
         PhotonNetwork.LoadLevel("WaitingRoom");
     }
 
-    public void leaveRoom(){
-        PhotonNetwork.LeaveRoom();
-        PhotonNetwork.LoadLevel("StartMenu");
-    }
-
+   
     public void LeaveLobby(){
         PhotonNetwork.LeaveLobby();
     }
